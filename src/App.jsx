9f1e065a0a1154bbf348b0faafd6599507d85b8a
A7medmasry <ahmedmasry187@gmail.com>
2024-01-22 23:58:46 +0200

    fixed project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import LayOut from './Component/LayOut/LayOut'
+import{createBrowserRouter , RouterProvider} from 'react-router-dom'
+import About from './Component/About/About'
+import Gallery from './Component/Gallery/Gallery'
+import Home from './Component/Home/Home'
+import NotFound from './Component/NotFound/NotFound'
+
+
+let routers = createBrowserRouter([{
+  path:"",element:<LayOut/>,children:[
+    {index:true,element:<Gallery/>},
+    {path:"about",element:<About/>},
+    {path:"home",element:<Home/>},
+    {path:"*",element:<NotFound/>}
+  ]
+}])
+export default function App() {
+  return (
+<RouterProvider router={routers}/>
+  )
+}
